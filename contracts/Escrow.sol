fd07848f4d4f95939754cf0c4a621f582c02e090
Franklin <franklin.op@hotmail.com>
2024-02-14 16:32:01 +0100

    all 14 test functionalities passed

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -88,4 +88,18 @@ contract Escrow {
     function getBalance() public view returns(uint256){
         return address(this).balance;
     }
+
+    
+    function finalizeSale(uint256 _nftID) public {
+        require(inspectionPassed[_nftID]);
+        require(approval[_nftID][buyer[_nftID]]);
+        require(approval[_nftID][seller]);
+        require(approval[_nftID][lender]);
+        require(address(this).balance >= purchasePrice[_nftID]);
+
+        (bool success, )= payable(seller).call{value:address(this).balance}("");
+        require(success);
+
+        IERC721(nftAddress).transferFrom(address(this), buyer[_nftID], _nftID);
+    }
 }
